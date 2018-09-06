900bd120a3c45c723c5d49af05837535bcf855b9
Zetnavrek <vykovi@gmail.com>
2018-09-06 12:15:19 -0500

    Updated and prepared

---
+++ README.md
@@ -0,0 +1,7 @@
+# Insta-Collage-Gallery
+
+A drag and drop project with JavaScript's API and Firebase. 
+
+## Elaborado para: 
+
+[Laboratoria](http://www.laboratoria.la/)
--- js/auth.js
+++ js/auth.js
@@ -1,50 +1,51 @@
 
-    $(document).ready(() => {
-        $("#login").click(() => {
-            let email = $("#email").val();
-            let password = $("#password").val();
-     
-            firebase.auth().signInWithEmailAndPassword(email, password)
-                .catch((error) => {
-                    swal(error.message);
-                })
-     
+$(document).ready(() => {
+    $("#login").click(() => {
+        let email = $("#email").val();
+        let password = $("#password").val();
+        
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch((error) => {
+            swal(error.message);
         })
-        $("#logout").click(() => {
-            firebase.auth().signOut();
-            window.location = "index.html";
-        })
-     
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                $("#user").text(user.email);
-                console.log(window.location.pathname);
-                if(!window.location.pathname.match(/gallery.html$/)){
-                    window.location = "gallery.html";
-                }
-            } else {
-                $("#user").text("");
+        
+    })
+    $("#logout").click(() => {
+        firebase.auth().signOut();
+        window.location = "index.html";
+    })
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            $("#user").text(user.email);
+            console.log(window.location.pathname);
+            if(!window.location.pathname.match(/gallery.html$/)){
+                window.location = "gallery.html";
             }
-        })
-     
-     
-        $("#signup").click(() => {
-            let email = $("#email").val();
-            let password = $("#password").val();
-     
-            if (password == 123456) {
-                swal("Error ", " The password can not be 123456, empty field or have less than 6 characters", "error");
-                let passwordInput = $("#password").val("");
-            } else {
-                firebase.auth().createUserWithEmailAndPassword(email, password)
-                    .catch((error) => {
-                        //swal("Error", error.message, "error");
-                    })
-            }
-     
-     
-     
-        })
-     });
+            
+        } else {
+            $("#user").text("");
+        }
+    })
     
     
+    $("#signup").click(() => {
+        let email = $("#email").val();
+        let password = $("#password").val();
+        
+        if (password == 123456) {
+            swal("Error ", " The password can not be 123456, empty field or have less than 6 characters", "error");
+            let passwordInput = $("#password").val("");
+        } else {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch((error) => {
+                //swal("Error", error.message, "error");
+            })
+        }
+        
+        
+        
+    })
+});
+
+
